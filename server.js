583dc681827a4f0cf0ceec110e2217155afe16b4
harsha163 <harshavardhan163@gmail.com>
2016-09-28 19:00:46 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -112,7 +112,7 @@ app.get('/ui/style.css', function (req, res) {
 });
 app.get('/:articleName', function(req, res){
     var articleName = req.params.articleName;
-   res.send(createTemplate(articles[articleNaame]));
+   res.send(createTemplate(articles[articleName]));
 });
 
 
