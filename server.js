c21b924f7548776e833fd5fdf4703e63da361fa7
harsha163 <harshavardhan163@gmail.com>
2016-09-28 16:09:10 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -12,6 +12,17 @@ app.get('/', function (req, res) {
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
+app.get('/article-1', function(req, res){
+   res.send('art1'); 
+});
+
+app.get('/article-2', function(req, res){
+   res.send('art2'); 
+});
+
+app.get('/article-3', function(req, res){
+   res.send('art3'); 
+});
 
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
