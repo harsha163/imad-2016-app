569b9a7f52877ecd55322e93e802bb4120fa9e27
harsha163 <harshavardhan163@gmail.com>
2016-09-30 19:22:41 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -115,7 +115,7 @@ app.get('/:articleName', function(req, res){
    res.send(createTemplate(articles[articleName]));
 });
 var counter=0;
-app.get('/:articleName', function(req, res){
+app.get('/counter', function(req, res){
     counter=counter+1;
     res.send(counter.toString());
 });
