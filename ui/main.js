53970c2747f85e6c6509f80ba218ff8d9622071c
harsha163 <harshavardhan163@gmail.com>
2016-09-30 22:42:48 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -28,3 +28,17 @@ button.onclick = function(){
     req.send(null);
     
 };
+
+
+var nameIn=document.getElementById('name');
+var nameg = nameIn.value;
+var submitButon=document.getElementById("submit");
+submitButon.onclick = function(){
+    var names=['name1','name2','name3'];
+    var list='';
+    for(var i=0;i<names.length;i++){
+        list+='<li>'+names[i]+'</li>';
+    }
+    var ul=document.getElementById('namesList');
+    ui.innerHTML = list;
+};
