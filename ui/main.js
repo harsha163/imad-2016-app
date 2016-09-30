d079e04d42c789909ecb9e44d7eff763c94e6efe
harsha163 <harshavardhan163@gmail.com>
2016-09-30 20:04:29 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -11,13 +11,17 @@ madi.onclick = function(){
     
     var interval = setInterval(moveRight,40);
 };
-var counter=0;
+var req = new XMLHttpRequest();
+
 var sp= document.getElementById("count");
 var button =document.getElementById("counter");
 button.onclick = function(){
-    
-    
-    counter=counter+1;
-    
-    sp.innerHTML= counter.toString();
+    req.onreadystatechange = function(){
+        if(req.readyState===XMLHttpRequest.Done){
+            if(req.status===200){
+                var counter = req.responseText;
+                sp.innerHTML = counter.toString();
+            }
+        }
+    };
 };
