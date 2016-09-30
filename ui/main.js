8fa90836374d72990e14bef5517aa7ad9a821c44
harsha163 <harshavardhan163@gmail.com>
2016-09-30 20:11:01 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -24,7 +24,7 @@ button.onclick = function(){
             }
         }
     };
-    req.open('GET',"http://harsha163.imad.hasura-app.io",true);
+    req.open('GET',"http://harsha163.imad.hasura-app.io/counter",true);
     req.send(null);
     
 };
