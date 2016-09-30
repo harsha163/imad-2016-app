f3fbef195be5ccfaa84654175159221e4c052fba
harsha163 <harshavardhan163@gmail.com>
2016-09-30 19:40:17 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,4 +1,4 @@
-console.log('Loaded!');
+
 
 var madi = document.getElementById('madi');
 var marginLeft= 0;
@@ -11,3 +11,11 @@ madi.onclick = function(){
     
     var interval = setInterval(moveRight,40);
 };
+var button =document.getElementById('counter');
+button.onclick = function(){
+    
+    
+    counter=counter+1;
+    var sp= document.getElementById('count');
+    sp.innerHtml= counter;
+}
